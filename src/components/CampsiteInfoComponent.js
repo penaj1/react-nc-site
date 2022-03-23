12483dd4d36c0b5f82a9a53ec3baa8652732c08b
Janice Pena <jipena94@gmail.com>
2022-03-22 19:13:54 -0700

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -43,8 +43,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    console.log('Current State is: ' + JSON.stringify(values));
-    alert('Current State is: ' + JSON.stringify(values));
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -127,7 +126,7 @@ function RenderCampsite({campsite}) {
   );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -148,7 +147,7 @@ function RenderComments({comments}) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -173,7 +172,12 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments 
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id}
+        />
+
         </div>
       </div>
     );
